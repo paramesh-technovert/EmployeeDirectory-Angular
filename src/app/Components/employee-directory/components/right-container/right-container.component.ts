407d73c4536499f5b5a9e525788f596d2b30bb4d
Sandeep Dangeti <sdangeti@mtm-inc.net>
2025-02-03 16:50:47 +0530

    latest

--- src/app/Components/employee-directory/components/right-container/right-container.component.ts
+++ src/app/Components/employee-directory/components/right-container/right-container.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-right-bar',
-  templateUrl: './right-Container.component.html',
-  styleUrls: ['./right-Container.component.css']
+  templateUrl: './right-container.component.html',
+  styleUrls: ['./right-container.component.css']
 })
 export class RightContainerComponent {
 }
